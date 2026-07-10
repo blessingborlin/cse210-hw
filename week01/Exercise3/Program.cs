c13d299303a051c6128a02160543a04edb780bcb
blessingborlin <bborlin@byupathway.edu>
2026-07-10 19:59:16 +0100

    commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,38 +1,36 @@
 using System;
+using System.Runtime.Serialization;
+using Microsoft.Win32.SafeHandles;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // For Parts 1 and 2, where the user specified the number...
-        // Console.Write("What is the magic number? ");
-        // int magicNumber = int.Parse(Console.ReadLine());
         
-        // For Part 3, where we use a random number
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
+         Console.Write("What is the magic number? ");
+         int magicNumber = int.Parse(Console.ReadLine());
 
-        int guess = -1;
+         
 
-        // We could also use a do-while loop here...
-        while (guess != magicNumber)
+         Console.Write("what is your guess?  ");
+         int guess = int.Parse(Console.ReadLine());
+
+
+        if (guess < magicNumber)
         {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
+          Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
 
-            if (magicNumber > guess)
-            {
-                Console.WriteLine("Higher");
-            }
-            else if (magicNumber < guess)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
+        else
+        {
+            Console.WriteLine("Awesome, you got it correctly!.");
+        }
+    }    
 
-        }                    
-    }
-}
+
+        
+}   
