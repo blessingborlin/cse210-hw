425ebd06e73d1df9ba847a89a1cdb6c1bb149727
blessingborlin <bborlin@byupathway.edu>
2026-07-11 19:08:55 +0100

    commit

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        Console.WriteLine(job1._company);
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Apple";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Console.WriteLine(job2._company);
     }
 }
