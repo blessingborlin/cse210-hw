4fe42b3c326dc3cb37b83761778bc593a680bf49
Blessing Borlin <bborlin@byupathway.edu>
2026-08-02 21:12:31 +0100

    commit

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -1,66 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
 public class Activity
 {
     private string _name;
     private string _description;
-    private int  _duration;
+    protected int _duration;
 
-    public Activity(string name, string description , int duration)
+    public Activity(string name, string description)
     {
         _name = name;
-        _description= description;
-        _duration=duration;
+        _description = description;
     }
+
     public void DisplayStartingMessage()
     {
-        Console.WriteLine($" Welcome to {_name}  Activity.");
+        Console.Clear();
+        Console.WriteLine($"Welcome to the {_name} Activity");
+        Console.WriteLine();
         Console.WriteLine(_description);
-    
+        Console.WriteLine();
+        Console.Write("How long, in seconds, would you like for your session? ");
+        _duration = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Prepare to begin...");
+        ShowSpinner(3);
     }
+
     public void DisplayEndingMessage()
     {
-        Console.WriteLine($"Thanks for finishing {_name}  Activity.");
-        Console.WriteLine(_description);
+        Console.WriteLine("Well done!!");
+        ShowSpinner(3);
+        Console.WriteLine($"You have completed another {_duration} seconds of the {_name} Activity.");
+        ShowSpinner(3);
     }
-    
-    public void ShowSpinner(int seconds )
-    {
-        List <string> spinner = new List<string> {"|" ,"/", "-", "\\" };
-        DateTime endtime = DateTime.Now.AddSeconds(seconds);
 
-        while (DateTime.Now < endtime)
+    public void ShowSpinner(int seconds)
+    {
+        List<string> spinner = new List<string> { "|", "/", "-", "\\" };
+        DateTime endTime = DateTime.Now.AddSeconds(seconds);
+        int i = 0;
+        while (DateTime.Now < endTime)
         {
-            foreach (string symbol in spinner)
-            Console.Write(symbol);
+            Console.Write(spinner[i]);
             Thread.Sleep(250);
             Console.Write("\b \b");
-
-            if (DateTime.Now >= endTime)
-            {
-                break;
-            }
+            i = (i + 1) % spinner.Count;
         }
     }
-    
-    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public void ShowCountDown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
 }
--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -1,17 +1,52 @@
-public class ListingActivity
+using System;
+using System.Collections.Generic;
+
+public class ListingActivity : Activity
 {
-    private int _count;
-    private List<string> _prompts;
+    private List<string> _prompts = new List<string> {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
 
-    public ListingActivity(int _count, List<string> _prompts)
+    private Random _random = new Random();
+
+    public ListingActivity() : base("Listing",
+        "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.") { }
+
+    public void Run()
     {
-        _count = count;
-        _prompts = prompts;
+        DisplayStartingMessage();
+        Console.WriteLine("List as many responses as you can to the following prompt:");
+        Console.WriteLine($"--- {GetRandomPrompt()} ---");
+        Console.Write("You may begin in: ");
+        ShowCountDown(5);
+
+        List<string> items = GetListFromUser();
+        Console.WriteLine($"You listed {items.Count} items!");
+        DisplayEndingMessage();
     }
-    
 
+    public string GetRandomPrompt()
+    {
+        return _prompts[_random.Next(_prompts.Count)];
+    }
 
+    public List<string> GetListFromUser()
+    {
+        List<string> items = new List<string>();
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+        Console.WriteLine();
 
-
-
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            string input = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(input))
+                items.Add(input);
+        }
+        return items;
+    }
 }
