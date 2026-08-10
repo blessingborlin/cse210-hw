70acee9c0d9d2070e72dfbece65bb62e58344f55
Blessing Borlin <bborlin@byupathway.edu>
2026-08-10 21:37:31 +0100

    commit

---
+++ week07/Running.cs
@@ -0,0 +1,55 @@
+using System.Diagnostics;
+
+public class Running : Activity
+{
+    private double  _distance;
+
+    public Running(int date, int length,double distance): base( date, length)
+    {
+        _distance = distance;
+    }
+    public override GetDistance()
+    {
+        return _distance;
+
+    }
+    public override double GetSpeed()
+    {
+        return _distance / GetLength() * 60;
+    }
+    public override double GetPace()
+    {
+      return GetLength()/_distance;
+    
+    }
+    protected override string GetActivityName()
+    {
+        return "Running";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
