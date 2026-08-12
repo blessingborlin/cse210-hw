93c4eb344f2e4ee1f5c7e7ad951609554c3418a9
Blessing Borlin <bborlin@byupathway.edu>
2026-08-12 19:21:51 +0100

    commit

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -2,24 +2,24 @@ public  abstract class Activity
 {
     private DateTime _date;
 
-    private int _length;
+    private int _Length;
 
     public Activity(DateTime date, int length)
     {
         _date = date;
-        _length = length;
+        _Length = length;
       
     }
     protected string GetLength()
     {
-        return _length;
+        return _Length;
     }
     public  abstract double GetDistance();
     public abstract double GetSpeed();
     public abstract double GetPace();
     public abstract string GetSummary()
      {
-        return $"{_date:dd MMM yyyy} {GetActivityName()} ({_length} min): " +
+        return $"{_date:dd MMM yyyy} {GetActivityName()} ({_Length} min): " +
                $"Distance {GetDistance():0.0} miles, " +
                $"Speed: {GetSpeed():0.0} mph, " +
                $"Pace: {GetPace():0.0} min per mile";
